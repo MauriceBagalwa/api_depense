323d3b478709a395270c415943a37b801df668a7
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-12-22 13:50:19 +0100

    feat(routera): add middleware for image

--- src/routers/entreprise-router.js
+++ src/routers/entreprise-router.js
@@ -1,8 +1,9 @@
 const entrepriseCtr = require("../mongo/controllers/entreprise-controller");
 const router = require("express").Router();
+const upload=require('../middleware/upload')
 
 router.get("/", entrepriseCtr.entreprisesOperation);
-router.post("/", entrepriseCtr.entreprise);
+router.post("/", upload.single('avatar'), entrepriseCtr.entreprise);
 router.put("/", entrepriseCtr.updateEntreprise);
 
 module.exports = router;
