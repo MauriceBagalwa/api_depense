6746b4e39f7a2a587e3206a502d4e9303b6a7607
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-12-25 14:22:33 +0100

    refactor(routers): add router (email)

--- src/routers/entreprise-router.js
+++ src/routers/entreprise-router.js
@@ -1,8 +1,12 @@
 const entrepriseCtr = require("../mongo/controllers/entreprise-controller");
 const router = require("express").Router();
-const upload=require('../middleware/upload')
+const upload = require("../middleware/upload");
+
 
 router.get("/", entrepriseCtr.entreprisesOperation);
+router.get("/code", entrepriseCtr.verifyCode);
+router.get("/mail", entrepriseCtr.sendEmail);
+
 router.post("/", entrepriseCtr.entreprise);
 router.put("/", entrepriseCtr.updateEntreprise);
 
