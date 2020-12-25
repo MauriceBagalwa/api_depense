5b0658c1b17ca775f35cd7fc08b6daaf708f7855
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-12-25 14:27:33 +0100

    refactor: test code for send mail

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyparser = require("body-parser");
 const morgan = require("morgan");
 const CreatError = require("http-errors");
 const router = require("./src/routers");
+const nodemailer = require("nodemailer");
+const nodemailerMailgum = require("nodemailer-mailgun-transport");
+
 require("./src/mongo/utils/db");
 require("dotenv").config();
 
@@ -13,7 +16,7 @@ app.use(bodyparser.json());
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Depense_App...");
+  res.send("Welcome to Depense_App... ");
 });
 
 /*
@@ -37,6 +40,41 @@ app.use((err, req, res, next) => {
     },
   });
 });
+
+/*
+? Sendted mail
+*/
+
+// /* --------------------------------- Etape 1 -------------------------------- */
+
+// const auth = {
+//   auth: {
+//     api_key: "",
+//     domaine: "",
+//   },
+// };
+// /* --------------------------------- Etape 2 -------------------------------- */
+// let transporter = nodemailer.createTransport(nodemailerMailgum(auth));
+
+// const mailOptions = {
+//   from: '"Test mail" <mauricebagalwa009@gmail.com>',
+//   to: "dev.maurice1317@gmail.com",
+//   subject: "Code de confirmation",
+//   text: "votre code de confitmation est 237899",
+// };
+
+// transporter.sendMail(mailOptions, function (err, data) {
+//   if (err) {
+//     res.send({
+//       status: 400,
+//       message: "Adress mail incorrect",
+//     });
+//   } else {
+//   }
+// });
+/*
+? Sendted mail
+*/
 app.listen(PORT, () => {
   console.log(`server in runing at 🚀 http://localhost:${PORT}`);
 });
