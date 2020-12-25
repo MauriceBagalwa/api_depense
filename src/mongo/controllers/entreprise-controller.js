b5dcf4ec40747df787513decde15fd6ce9ca7396
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-12-25 14:17:58 +0100

    refactor(controllers): update some functions

--- src/mongo/controllers/entreprise-controller.js
+++ src/mongo/controllers/entreprise-controller.js
@@ -1,5 +1,14 @@
 const Entreprise = require("../schemas/entreprise-Schema");
 const UserSchema = require("../schemas/user-schema");
+const createError = require("http-errors");
+const { sendEmail } = require("../utils/elementary");
+
+var randomFixedInteger = function (length) {
+  return Math.floor(
+    Math.pow(10, length - 1) +
+      Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1)
+  );
+};
 
 module.exports = {
   /* --------------------------- get all entreprise --------------------------- */
@@ -48,78 +57,73 @@ module.exports = {
     });
   },
 
-  changeEtat: (entreprise, res, next) => {
-    Entreprise.findById((_id = entreprise)).then((find) => {
-      console.log(find);
-      if (find)
-        find
-          .update({ created: true })
-          .then(function () {
-            res.status(200).json({
-              message: "Entreprise created...",
-            });
-          })
-          .catch((error) => {
-            next(error);
+  changeEtat: async (entreprise, res, next) => {
+    const find = await Entreprise.findById((_id = entreprise));
+    if (!find) throw createError.NotFound("Entreprise not created");
+    else {
+      await find
+        .updateOne({ created: true, code: randomFixedInteger(6) })
+        .then(function () {
+          res.status(200).json({
+            message: "Entreprise created...",
           });
-      else {
-        res.status(400).json({
-          message: "Entreprise not created",
+        })
+        .catch((error) => {
+          next(error);
         });
-      }
-    });
+    }
   },
   /* --------------------------- add new entreprise --------------------------- */
 
-  entreprise: (req, res, next) => {
+  entreprise: async (req, res, next) => {
     const { name, rccm, mail, number, adresse, user } = req.body;
-    Entreprise.findOne({
+
+    const find = await Entreprise.findOne({
       $and: [{ created: true }, { $or: [{ name }, { mail }] }],
-    }).then((find) => {
-      if (find)
-        res.status(400).json({
-          message: "Entreprise or email address already exist.",
-        });
-      else {
-        const values = new Entreprise({
-          name: name,
-          rccm: rccm,
-          mail: mail,
-          number: number,
-          adresse: adresse,
-        });
+    }).catch((error) => {});
 
-        values
-          .save()
-          .then((created) => {
-            user.entreprise = created._id;
+    if (find)
+      res.status(400).json({
+        message: "Entreprise or email address already exist.",
+      });
+    else {
+      
+      const entreprise = new Entreprise({
+        name: name,
+        rccm: rccm,
+        mail: mail,
+        number: number,
+        adresse: adresse,
+      });
 
-            UserSchema.findOne({
-              $and: [{ entreprise: user.entreprise }, { email: user.email }],
-            }).then((find) => {
-              if (find) {
-                res.status(400).json({
-                  message: "Entreprise no Found.",
+      await entreprise
+        .save()
+        .then((created) => {
+          user.entreprise = created._id;
+          UserSchema.findOne({
+            $and: [{ entreprise: user.entreprise }, { email: user.email }],
+          }).then((find) => {
+            if (find)
+              throw createError.Conflict(
+                "User or email address already exist."
+              );
+            else {
+              new UserSchema(user)
+                .save()
+                .then(() => {
+                  const use = module.exports;
+                  use.changeEtat(user.entreprise, res, next);
+                })
+                .catch((error) => {
+                  next(error);
                 });
-              } else {
-                new UserSchema(user)
-                  .save()
-                  .then(() => {
-                    const use = module.exports;
-                    use.changeEtat(user.entreprise, res, next);
-                  })
-                  .catch((error) => {
-                    next(error);
-                  });
-              }
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-            next(error);
+            }
           });
-      }
-    });
+        })
+        .catch((error) => {
+          next(error);
+        });
+    }
   },
 
   /* ------------------------ Rentre l'entreprise invibles ----------------------- */
@@ -147,5 +151,34 @@ module.exports = {
       .catch((error) => {
         next(error);
       });
+  },
+
+  /* ------------------------ verify code confirmation ------------------------ */
+  verifyCode: async (req, res, next) => {
+    console.log(req.body);
+    await Entreprise.findOne({ _id: req.body.entreprise, code: req.body.code })
+      .then((verify) => {
+        if (verify)
+          res.status(200).json({
+            message: "correct code.",
+          });
+        else throw createError.NotFound("Incorrect code.");
+      })
+      .catch((error) => {
+        next(error);
+      });
+  },
+  sendEmail: async (req, res, next) => {
+    await Entreprise.findOne({ _id: req.body.entreprise }).then((find) => {
+      if (find) {
+        const values = {
+          to: find.mail,
+          subject: "Test mail api",
+          message: `Votre code de confirmation est ${find.code}`,
+        };
+        console.log(values);
+        sendEmail(values, res, next);
+      } else throw createError.NotFound("Email address not registered.");
+    });
   },
 };
