5ca9ac3c46cc2ddbc09cce6b0ee5cde552c75afd
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-12-27 01:43:23 +0100

    feat(router): add upadtemail router

--- src/routers/entreprise-router.js
+++ src/routers/entreprise-router.js
@@ -2,12 +2,13 @@ const entrepriseCtr = require("../mongo/controllers/entreprise-controller");
 const router = require("express").Router();
 const upload = require("../middleware/upload");
 
-
 router.get("/", entrepriseCtr.entreprisesOperation);
 router.get("/code", entrepriseCtr.verifyCode);
 router.get("/mail", entrepriseCtr.sendEmail);
 
 router.post("/", entrepriseCtr.entreprise);
+router.post("/updatemail", entrepriseCtr.updateMail);
+
 router.put("/", entrepriseCtr.updateEntreprise);
 
 module.exports = router;
