d6844f716cc5d74a9276ee4411518242c1fe9ab8
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-12-28 15:15:30 +0100

    feat(utils): add functon middleware

--- src/mongo/utils/elementary.js
+++ src/mongo/utils/elementary.js
@@ -1,5 +1,7 @@
 const sendmail = require("sendmail");
 const nodemailer = require("nodemailer");
+const Entreprise = require("../schemas/entreprise-Schema");
+const createError = require("http-errors");
 require("dotenv").config();
 
 module.exports = {
@@ -26,5 +28,18 @@ module.exports = {
           message: `Mail send to ${info.response}`,
         });
     });
+  },
+  verifyMailEntreprise: async (req, res, next) => {
+    try {
+      console.log(req.query);
+      const find = Entreprise.findOne({ mail: req.query.mail });
+      if (find) createError.Conflict(`Incorrect mail`);
+      else
+        res.send({
+          message: "Autentique",
+        });
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
