55d7649032e8912e4d0885fa6e3dea344cae2e8b
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-12-28 15:17:28 +0100

    feat(routers): add milldawer

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,9 @@
 const userRouter = require("./user-router");
 const entrepriseRouter = require("./entreprise-router");
+const middleware = require("./middleware");
 
 module.exports = {
   userRouter,
   entrepriseRouter,
+  middleware,
 };
