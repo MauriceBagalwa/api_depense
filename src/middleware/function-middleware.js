820176a5cec470c824d7717ecec35f95bd533ac1
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2021-01-29 12:40:10 +0100

    feat(middleware) add get's middleware

--- src/middleware/function-middleware.js
+++ src/middleware/function-middleware.js
@@ -1,14 +1,36 @@
 const db = require("../mongo/schemas/function-schema");
 module.exports = {
   isExist: async (req, res, next) => {
-    db.findOne({ designation: req.body.designation })
+    const { entreprise, designation } = req.body;
+    await db
+      .findOne({ entreprise, designation })
       .then((find) => {
         if (find) {
-          res.send({
+          res.status(409).json({
             code: 409,
             message: "Function already exist.",
           });
-        } else next();
+        } else {
+          next();
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  },
+  isExistUpdate: async (req, res, next) => {
+    const { entreprise, designation, description } = req.body;
+    await db
+      .findOne({ entreprise, designation, description })
+      .then((find) => {
+        if (find) {
+          res.status(409).json({
+            code: 409,
+            message: "Function already exist.",
+          });
+        } else {
+          next();
+        }
       })
       .catch((err) => {
         next(err);
