0a35c2e84236f0f03a2ed2215688be45de7efee6
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2021-01-29 12:42:38 +0100

    feat(controller): add post's & get's controller

---
+++ src/mongo/controllers/operation.js
@@ -0,0 +1,83 @@
+const db = require("../schemas/autorization_schema");
+module.exports = {
+  /* ------------------------- #selection des opération ------------------------ */
+
+  Operations: async (req, res, next) => {
+    await db
+      .find({ entreprise: req.body.entreprise })
+      .then((finds) => {
+        res.send({
+          status: 200,
+          message: finds,
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  },
+
+  /* ------------------- #ajout d'un nouveau type d'operation ------------------ */
+
+  Operation: async (req, res, next) => {
+    const values = new db({
+      designation: req.body.designation,
+      entreprise: req.body.entreprise,
+      users: req.body.users,
+    });
+    await values
+      .create(values)
+      .then(() => {
+        const use = module.exports;
+        use.typeOperation(req, res, next);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  },
+
+  /* -------------------------------------------------------------------------- */
+  /*                    #Moidification d'un type d'operation                    */
+  /* -------------------------------------------------------------------------- */
+
+  update: async (req, res, next) => {
+    const filter = { _id: req.body._id };
+    const update = { designation: req.body.designation };
+    await db
+      .findOneAndUpdate(filter, update, { returnOriginal: false })
+      .then((updated) => {
+        res.send({
+          status: 200,
+          message: updated,
+        });
+      });
+  },
+
+  UpdateUser: async (req, res, next) => {
+    const filter = { _id: req.body._id };
+    const update = { users: req.body.designation };
+    await db
+      .findOneAndUpdate(filter, update, { returnOriginal: false })
+      .then((updated) => {
+        res.send({
+          status: 200,
+          message: updated,
+        });
+      });
+  },
+
+  /* -------------------------------------------------------------------------- */
+  /*                                 #Suppresion                                */
+  /* -------------------------------------------------------------------------- */
+  deleteOperation: async (req, res, next) => {
+    db.findByIdAndDelete(req.params._id)
+      .then(() => {
+        res.send({
+          status: 200,
+          message: "Type Operation effacer avec succès.",
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  },
+};
