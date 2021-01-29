f00ba7aa25157d3a4433eedab8741172711fcdd8
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2021-01-29 12:46:35 +0100

    chore(utils): add config file

--- src/mongo/utils/db.js
+++ src/mongo/utils/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-
+const config = require("../utils/config");
 mongoose
   .connect(
-    "mongodb+srv://updev_:admin@user-delevery.pxvf0.mongodb.net/depense_db?retryWrites=true&w=majority",
+    // "mongodb+srv://updev_:admin@user-delevery.pxvf0.mongodb.net/depense_db?retryWrites=true&w=majority",
+    config.dburl,
     {
       useUnifiedTopology: true,
       useNewUrlParser: true,
@@ -12,5 +13,3 @@ mongoose
   .catch((err) => {
     console.log("DB Connection Error: " + err);
   });
-
-  
