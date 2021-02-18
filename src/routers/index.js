da55d1850c6f7430bd10e71a5578c10cc4355501
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2021-02-18 04:03:17 +0100

    feat(index): add operation class

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,12 +2,12 @@ const userRouter = require("./user-router");
 const entrepriseRouter = require("./entreprise-router");
 const middleware = require("./middleware");
 const functionRouter = require("./function-router");
-const autorization = require("./autorization-router");
+const operation = require("./operation");
 
 module.exports = {
   userRouter,
   entrepriseRouter,
   middleware,
   functionRouter,
-  autorization,
+  operation,
 };
